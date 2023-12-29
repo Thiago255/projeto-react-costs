caa7d3ecabcd7283484905d6a7b1d417e8e42b81
Thiago Blasi <thiago_blasi26@hotmail.com>
2023-12-29 11:43:45 -0300

    Mensagens do sistema

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,5 +1,27 @@
+import { useLocation } from 'react-router-dom'
+
+import Message from '../layout/Message'
+
 function Projects() {
-    return <h1>Project</h1>
+    
+    const location = useLocation()
+    let message = ''
+    if(location.state){
+        message= location.state.message
+    }
+
+    return (
+
+        <div>
+
+            <h1>Meus Projetos</h1>
+            
+            {message && <Message type='success' msg={message}/>}
+
+        </div>
+        
+    )
+
 }
 
 export default Projects
